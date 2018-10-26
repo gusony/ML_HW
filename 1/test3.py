8f9f8c18598bb1f094a129c94d00cee697f5db57
Junhan <junhan@g2.nctu.edu.tw>
2018-10-26 13:22:01 +0800

    update test3.py

--- 1/test3.py
+++ 1/test3.py
@@ -2,33 +2,3 @@ import numpy as np
 from numpy.linalg import inv
 import matplotlib.pyplot as plt
 
-def two_d_matrix_tolist(X):
-  temp = []
-  for i in X.tolist():
-    for j in i:
-      temp.append(j)
-  return(temp)
-
-X = np.matrix([[1]])
-print(two_d_matrix_tolist(X))
-
-'''
-X = [[1,2,3],[4,5,6],[7,8,9],[10,11,12],[151,156,126]]
-target_matrix = np.matrix([[13],[14],[15],[16],[20]])
-X = np.matrix(X)
-#print((X.T*X).I)
-def lamb_identity(lamb, shape):
-  result = []
-  for i in range(shape):
-    temp = []
-    for j in range(shape):
-      temp.append(lamb)
-    result.append(temp)
-  return(result)
-
-#W  = inv(X.T*X)*X.T*target_matrix
-
-
-W =  inv(lamb_identity(0.01,X.shape[1]) +X.T*X)*X.T*target_matrix
-print(W)
-'''
