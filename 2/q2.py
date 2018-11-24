6b44e15cb67d46406b1e428a9143eb323765d696
Junhan <junhan@g2.nctu.edu.tw>
2018-11-24 16:16:23 +0800

    upload q2.py

---
+++ 2/q2.py
@@ -0,0 +1,9 @@
+import csv  #read .csv
+import numpy as np
+from numpy.linalg import pinv
+from numpy.linalg import inv
+from sympy import *
+import matplotlib.pyplot as plt
+import numpy.linalg
+import math
+
