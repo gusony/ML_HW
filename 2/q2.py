350c0f953e8c1852e7937a566362bdb3193e1291
Junhan <junhan@g2.nctu.edu.tw>
2018-11-25 16:17:00 +0800

    readfile

--- 2/q2.py
+++ 2/q2.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt
 import numpy.linalg
 import math
 
+
+with open('train.csv', newline='') as trainfile:
+    rows = csv.reader(trainfile)
+    for row in rows:
+        Raw_data_x.append(float(row[0]))
+        Raw_data_t.append(float(row[1]))
