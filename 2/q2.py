dfc2036fca2bacb3e035e77f97ddf71b606c2318
Junhan <junhan@g2.nctu.edu.tw>
2018-11-29 13:30:31 +0800

    finish q2 part 5,6

--- 2/q2.py
+++ 2/q2.py
@@ -18,7 +18,8 @@ x = []
 EW_list = []
 test_feature = []
 
-ε = 0.5
+ε = 10
+ε2 = 50
 
 #############################################################################
 def init_W_matrix():
@@ -44,7 +45,27 @@ def get_y_matrix(a):
     return(np.matrix(result,dtype=np.float64))
 
 def error_func(t,y):
-    return(sum((-1)*t.item(n,k)*np.log(float(y.item(n,k))) for n in range(t.shape[0]) for k in range(t.shape[1]) ) )
+    sum = 0
+    for n in range(t.shape[0]):
+        for k in range(t.shape[1]):
+            if y.item(n,k) < 1.0e-300 :
+                sum += (-1)*t.item(n,k)*np.log(float(1.0e-300))
+            else:
+                sum += (-1)*t.item(n,k)*np.log(float(y.item(n,k)))
+    return(sum)
+
+def find_most_contri_vari(w):
+    new_t = np.matrix(raw_class_result)
+    error_list = []
+    new_phi = np.matrix([])
+    for skip_item in range(w.shape[1]): #w.shape = (3, 7)
+        new_phi = np.delete(np.matrix(raw_feature).T, [skip_item],0).T
+        new_w   = np.delete(w.T,                      [skip_item],0).T
+        new_a = new_w.dot(new_phi.T)
+        new_y = get_y_matrix(new_a)
+        error_list.append(error_func(new_t, new_y))
+    return(error_list)
+
 
 def get_R_matrix(y):
     array_y = np.array(y)
@@ -111,7 +132,7 @@ f,part1 = plt.subplots()
 part1.set_title('HW2 Q2 part1, E(w)')
 part1.set_xlabel('iteration times')
 part1.text(round(i/2),170,"ε=0.5\ninteration times ="+str(i))
-part1.scatter(x, EW_list)
+part1.plot(x, EW_list)
 
 #part2
 class_result_matrix = get_class_result(get_y_matrix(W_matrix.dot(test_feature.T)))
@@ -132,17 +153,44 @@ part3.hist(np.array(raw_feature)[:,5],label='f[5]')
 part3.hist(np.array(raw_feature)[:,6],label='f[6]')
 plt.legend()
 
+#part5
+f,part5_1 = plt.subplots()
+f,part5_2 = plt.subplots()
 
-f,part5 = plt.subplots()
-part5.scatter(np.array(raw_feature)[:,3], np.array(raw_feature)[:,6])
-part5.set_title('HW2 Q2 part5')
+part5_2.set_title('HW2 Q2 part5 ,find most contribution variable')
+part5_2.set_xlabel("index of x which isn't included" )
+part5_2.set_ylabel('E(W)')
+part5_2.plot(np.arange(1,8), find_most_contri_vari(W_matrix))
+
+part5_1.scatter(np.array(raw_feature)[:,0], np.array(raw_feature)[:,3])
+part5_1.set_title('HW2 Q2 part5')
 
 
+#part 6
+p6_w = np.array(list(0 for i in range(3*2))).reshape(3,2)
+p6_phi = np.delete(np.matrix(raw_feature).T, [1,2,4,5,6],0).T
 
+EW_list2 = []
+while True:
+    p6_a_m = p6_w.dot(p6_phi.T)
+    p6_y_m = get_y_matrix(p6_a_m)
+    p6_w = update_w_mtx(p6_phi, p6_w, t_matrix, p6_y_m)
+    EW_list2.append(error_func(t_matrix, p6_y_m))
+    print(len(EW_list2))
+    if EW_list2[len(EW_list2)-1] < ε2:
+        break
 
+f,part6 = plt.subplots()
+part6.set_title('Q2 part6,E(W)')
+part6.set_ylabel('E(w)')
+part6.plot(np.arange(len(EW_list2)).tolist(), EW_list2)
 
+f,part6_2 = plt.subplots()
+class_result_matrix = get_class_result(get_y_matrix(p6_w.dot( np.delete(test_feature.T, [1,2,4,5,6],0 ))))
+class_result_list = np.array(class_result_matrix.dot(np.matrix([[1],[2],[3]]))).flatten().tolist()
 
-
+part6.set_title('Q2 part6, test data, class result')
+part6_2.scatter(list(range(class_result_matrix.shape[0])),class_result_list)
 
 
 
