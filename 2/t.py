39d3bd8ed47acd70105b4b556325b3315fa19600
Junhan <junhan@g2.nctu.edu.tw>
2018-11-30 13:30:52 +0800

    test.py

---
+++ 2/t.py
@@ -0,0 +1,13 @@
+import csv  #read .csv
+import numpy as np
+from numpy.linalg import pinv
+from numpy.linalg import inv
+from sympy import *
+import matplotlib.pyplot as plt
+import numpy.linalg
+import math
+
+l = [4,5,6]
+print(type(np.arange(len(l))))
+#l = np.array(l).reshape(2,7)
+print(l)
