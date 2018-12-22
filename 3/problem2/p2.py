93939504b6a2395a5df8fc7de9c07e297f446aef
Junhan <junhan@g2.nctu.edu.tw>
2018-12-23 01:25:08 +0800

    test linear src ovr

--- 3/problem2/p2.py
+++ 3/problem2/p2.py
@@ -12,10 +12,16 @@ def readdata(textname):
 train_x = np.array(readdata("x_train.csv"), dtype=float)
 train_t = np.array(readdata("t_train.csv"), dtype=int).flatten()
 
+#create firgure
 f,flt = plt.subplots(2,2,sharex='all',sharey='all')
+#set boundary
 flt[0,0].set(xlim=[-1, 1], ylim=[-1, 1])
-
-
+#set titles
+flt[0,0].set_title('Linear SVC (ovr)')
+flt[0,1].set_title('Linear SVC (ovo)')
+flt[1,0].set_title('Poly SVC (degree=2,ovr)')
+flt[1,1].set_title('Poly SVC (degree=2,ovo)')
+#show data to all figure
 for i in range(2):
     for j in range(2):
         for n in range(len(train_t)):
@@ -26,24 +32,22 @@ for i in range(2):
             elif train_t[n] == 3 :
                 flt[i,j].scatter(train_x[n,0], train_x[n,1], color='g')
 
-flt[0,0].set_title('LinearSVC ovr')
+
 #clf = svm.SVC(kernel='linear', decision_function_shape='ovo')
 clf = svm.LinearSVC()
 clf.fit(train_x, train_t)
-#print(clf.support_vectors_.shape)
-#print(clf.n_support_)
-#print("clf.dual_coef_",clf.dual_coef_)
-print("clf.coef_\n",clf.coef_)
-print("clf.intercept_\n",clf.intercept_)
-#print(clf.decision_function(train_x))
-
 # one vs rest
 x = np.linspace(-1, 1, 1000)
 flt[0,0].plot(x, (-clf.coef_[0][0]*x + clf.intercept_[0])/clf.coef_[0][1], color='r')
 flt[0,0].plot(x, (-clf.coef_[1][0]*x + clf.intercept_[1])/clf.coef_[1][1], color='b')
 flt[0,0].plot(x, (-clf.coef_[2][0]*x + clf.intercept_[2])/clf.coef_[2][1], color='g')
 
-
+#print(clf.support_vectors_.shape)
+#print(clf.n_support_)
+#print("clf.dual_coef_",clf.dual_coef_)
+print("clf.coef_\n",clf.coef_)
+print("clf.intercept_\n",clf.intercept_)
+#print(clf.decision_function(train_x))
 
 
 
