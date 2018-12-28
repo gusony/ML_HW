172ea8f376362243249a3648a8d4c0c521332b14
Junhan <junhan@g2.nctu.edu.tw>
2018-12-28 13:16:41 +0800

    kmean OK

--- 3/problem3/p3.py
+++ 3/problem3/p3.py
@@ -7,29 +7,36 @@ import cv2
 import seaborn as sns; sns.set()
 from sklearn.cluster import KMeans
 
-max_interaction = 1000
+max_interaction = 10
 
 #input data, 2D [n data, features].
 #return k data of center[number of k, features]
 def keans(x, k_num):
     #init k
-    uk = [x[np.random.randint(len(x))] for i in range(k_num)]
+    uk = np.array([list(x[np.random.randint(len(x))].data) for i in range(k_num)])
 
     interaction = 0
-    labels = [] # result
-
     while interaction <= max_interaction:
+        interaction += 1
         print('interaction:',interaction,'\n-------------------')
-        for n in range(len(x)): #對於每一筆資料
-            labels.append(uk.indexOf(min([(x[n]-center)**2 for center in uk]))) #計算到每個群中心的距離 取最小的群中心
+        γ = []
 
+        #計算 每一個點對k個群中心的距離, 取最小的距離, 分到該群
+        for n in range(len(x)): #對於每一筆資料
+            temp = [0 for i in range(k_num)]
+            temp[np.argmin([np.sum((x[n]-center)**2) for center in uk])] = 1
+            γ.append(temp)
+        γ = np.matrix(γ)
+        uk = [ (γ.T.dot(x)[0,:] / sum(γ[:,0])).tolist()[0] for k in range(k_num)]
+    labels = [n.item(0) for n in γ]
+    return(labels)
 
 
 
 img = cv2.imread('hw3.jpg')
 
-X = img.flatten()
-print(X.shape)
+X = img.reshape(img.shape[0]*img.shape[1],img.shape[2])
+keans(X,2)
 #print(type(img[0][0]))
 #print(img[0])
 #print(img.shape)
@@ -49,11 +56,11 @@ print(X.shape)
 #col = col.astype(np.uint8)
 #print(col.dtype)
 #print(type(col), col.reshape(246,480,3).shape)
-newimg = col.reshape(246,480,3)
+#newimg = col.reshape(246,480,3)
 #print(newimg)
-cv2.imshow('test',newimg)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#cv2.imshow('test',newimg)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
 #print(img.shape[0], img.shape[1],labels.shape)
 #for i in range(img.shape[0]):
 #    for j in range(img.shape[1]):
